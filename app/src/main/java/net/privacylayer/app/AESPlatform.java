eb85c0b83f95ceda9a60d347c5239ec9d054eacf
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-08-29 23:50:32 +0200

    Fix AESPlatform, make it static

--- app/src/main/java/net/privacylayer/app/AESPlatform.java
+++ app/src/main/java/net/privacylayer/app/AESPlatform.java
@@ -1,16 +1,21 @@
 package net.privacylayer.app;
 
+import android.util.Base64;
+
 import java.io.UnsupportedEncodingException;
-import javax.crypto.*;
-import javax.crypto.spec.GCMParameterSpec;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.util.Arrays;
-import android.util.Base64;
 import java.util.Random;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class AESPlatform {
@@ -20,60 +25,69 @@ public class AESPlatform {
     public static final int GCM_NONCE_LENGTH = 12; // in bytes
     public static final int GCM_TAG_LENGTH = 16; // in bytes
 
-    KeyGenerator keyGen;
-    Cipher cipher;
-    GCMParameterSpec spec;
-
-    public AESPlatform() throws
-            NoSuchAlgorithmException,
-            NoSuchProviderException,
-            NoSuchPaddingException {
-        this(DEFAULT_AES_KEY_SIZE);
-    }
-
-    public AESPlatform(int aesKeySize) throws
-            NoSuchAlgorithmException,
-            NoSuchProviderException,
-            NoSuchPaddingException {
-        keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(aesKeySize);
-        cipher = Cipher.getInstance("AES/GCM/NoPadding");
-    }
-
-    AESMessage encrypt(String inputString, String key) throws
+    public static AESMessage encrypt(String inputString, String key) throws
             InvalidKeyException,
             InvalidAlgorithmParameterException,
             IllegalBlockSizeException,
             BadPaddingException,
             UnsupportedEncodingException,
-            NoSuchAlgorithmException {
+            NoSuchAlgorithmException,
+            NoSuchPaddingException {
+        return encrypt(inputString, key, DEFAULT_AES_KEY_SIZE);
+    }
+
+    public static AESMessage encrypt(String inputString, String keyString, int keySize) throws
+            InvalidKeyException,
+            InvalidAlgorithmParameterException,
+            IllegalBlockSizeException,
+            BadPaddingException,
+            UnsupportedEncodingException,
+            NoSuchAlgorithmException,
+            NoSuchPaddingException {
         byte[] input = inputString.getBytes("UTF-8");
         final byte[] nonce = new byte[GCM_NONCE_LENGTH];
         Random random = new Random();
         random.nextBytes(nonce);
 
-        byte[] encrypted = operate(Cipher.ENCRYPT_MODE, input, key, nonce);
+        byte[] encrypted = operate(Cipher.ENCRYPT_MODE, input, keyString, keySize, nonce);
         return new AESMessage(encrypted, nonce);
     }
 
-    String decrypt(AESMessage message, String keyString) throws
+    public static String decrypt(AESMessage message, String keyString) throws
             InvalidKeyException,
             InvalidAlgorithmParameterException,
             IllegalBlockSizeException,
             BadPaddingException,
             UnsupportedEncodingException,
-            NoSuchAlgorithmException {
-        byte[] decrypted = operate(Cipher.DECRYPT_MODE, message.content, keyString, message.nonce);
+            NoSuchAlgorithmException,
+            NoSuchPaddingException {
+        return decrypt(message, keyString, DEFAULT_AES_KEY_SIZE);
+    }
+
+    public static String decrypt(AESMessage message, String keyString, int keySize) throws
+            InvalidKeyException,
+            InvalidAlgorithmParameterException,
+            IllegalBlockSizeException,
+            BadPaddingException,
+            UnsupportedEncodingException,
+            NoSuchAlgorithmException,
+            NoSuchPaddingException {
+        byte[] decrypted = operate(Cipher.DECRYPT_MODE, message.content, keyString, keySize, message.nonce);
         return new String(decrypted, "UTF-8");
     }
 
-    private byte[] operate(int mode, byte[] input, String keyString, byte[] nonce)
+    private static byte[] operate(int mode, byte[] input, String keyString, int keySize, byte[] nonce)
             throws UnsupportedEncodingException,
             NoSuchAlgorithmException,
             InvalidKeyException,
             InvalidAlgorithmParameterException,
             IllegalBlockSizeException,
-            BadPaddingException {
+            BadPaddingException, NoSuchPaddingException {
+
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(keySize);
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
         byte[] key = keyString.getBytes("UTF-8");
 
         MessageDigest sha = MessageDigest.getInstance("SHA-1");
@@ -81,7 +95,7 @@ public class AESPlatform {
         key = Arrays.copyOf(key, 16); // use only first 128 bit
         SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
 
-        spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce);
+        GCMParameterSpec spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce);
         cipher.init(mode, secretKeySpec, spec);
 
 /*
@@ -96,10 +110,9 @@ public class AESPlatform {
 
     // Example implementation
     private static void example() throws Exception {
-        final AESPlatform t = new AESPlatform();
-        AESMessage encrypted = t.encrypt("We Ichi!", ":3");     // We 
+        AESMessage encrypted = AESPlatform.encrypt("We Ichi!", ":3");     // We
         System.out.println(encrypted.toString());
-        String decrypted = t.decrypt(encrypted, ":3");
+        String decrypted = AESPlatform.decrypt(encrypted, ":3");
         System.out.println(decrypted);
     }
 }
--- app/src/main/java/net/privacylayer/app/Encrypt.java
+++ app/src/main/java/net/privacylayer/app/Encrypt.java
@@ -1,16 +1,16 @@
 package net.privacylayer.app;
 
 import android.content.ClipData;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.ClipboardManager;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 public class Encrypt extends AppCompatActivity {
 
-    public String ENCKEY = "huehuehue";                                                              // Todo: implement custom keying
+    public String ENCKEY = "huehuehue";                                                             // Todo: implement custom keying
 
     public CharSequence encText;
     public boolean showToastOnEnc = true;
@@ -21,15 +21,43 @@ public class Encrypt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        CharSequence text = getIntent()
-                .getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);                                   // grabbin' the text!
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
-        boolean readonly = getIntent()
-                .getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false);                        // is our text from a readonly source?
+            String text = getIntent()
+                    .getStringExtra(Intent.EXTRA_PROCESS_TEXT);                                     // grabbin' the text!
 
+            boolean readonly = getIntent()
+                    .getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false);                    // is our text from a readonly source?
 
+            try {
+                encText = AESPlatform.encrypt(text, ENCKEY).toString();
+            } catch (Exception e) {
+                // Whoops, this shouldn't happen.
+                // TODO: add proper logic
+                return;
+            }
 
-        if (readonly) {
+            if (readonly) {
+                ClipData clip = ClipData.newPlainText("label", encText);                            // the clipboard now has the encrypted text inside of it
+                clipboard.setPrimaryClip(clip);                                                     // the encrypted text is now the primary clip in the clipboard
+                if (showToastOnEnc) {                                                               // We're only showing the toast if the user has the option enabled (as per default!)
+                    CharSequence toastContent = "Encrypted message copied to clipboard!";
+                    Toast toast = Toast.makeText(getApplicationContext(), toastContent, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            } else {
+                if (saveOldToClip) {                                                                // We're firing only if the
+                    ClipData clip = ClipData.newPlainText("label", text);                           // the clipboard now has the encrypted text inside of it
+                    clipboard.setPrimaryClip(clip);                                                 // the unencrypted text is now the primary clip in the clipboard
+                }
+                Intent intent = new Intent();
+                intent.putExtra(Intent.EXTRA_PROCESS_TEXT, encText);                                // Let's send back the encrypted text
+                setResult(RESULT_OK, intent);
+            }
+
+        } else {
+            CharSequence text = getIntent()
+                    .getStringExtra(Intent.ACTION_SEND);                                            // grabbin' the text!
             ClipData clip = ClipData.newPlainText("label", encText);                                // the clipboard now has the encrypted text inside of it
             clipboard.setPrimaryClip(clip);                                                         // the encrypted text is now the primary clip in the clipboard
             if (showToastOnEnc) {                                                                   // We're only showing the toast if the user has the option enabled (as per default!)
@@ -38,15 +66,6 @@ public class Encrypt extends AppCompatActivity {
                 toast.show();
             }
 
-
-        } else {
-            if (saveOldToClip) {                                                                    // We're firing only if the
-                ClipData clip = ClipData.newPlainText("label", text);                               // the clipboard now has the encrypted text inside of it
-                clipboard.setPrimaryClip(clip);                                                     // the unencrypted text is now the primary clip in the clipboard
-            }
-            Intent intent = new Intent();
-            intent.putExtra(Intent.EXTRA_PROCESS_TEXT, encText);                                    // Let's send back the encrypted text
-            setResult(RESULT_OK, intent);
         }
 
     }
