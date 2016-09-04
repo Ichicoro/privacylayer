87d9e9c3e3fd6cfe953655e3940ec2c46a50ebe2
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-09-04 15:02:16 +0200

    Remove additional button in KeyExchange

--- app/src/main/java/net/privacylayer/app/KeyExchange.java
+++ app/src/main/java/net/privacylayer/app/KeyExchange.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -88,16 +86,6 @@ public class KeyExchange extends AppCompatActivity {
                 }
             }
         });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
